ecf60e68cc601744d478a5d32139e1b3f4a81a45
renzon <renzon@gmail.com>
2015-08-25 10:39:44 -0300

    Aula sobre classes

---
+++ aula5/pessoa.py
@@ -0,0 +1,23 @@
+class Pessoa():
+    OLHOS = 2
+    def __init__(self, nome):
+        self.nome=nome
+
+    def cumprimentar(this):
+        return 'Olá, meu nome é %s'%this.nome
+
+pessoa = Pessoa('Renzo')
+denis = Pessoa('Denis')
+
+print(pessoa.nome)
+print(denis.nome)
+print(denis.cumprimentar())
+print(denis.cumprimentar())
+print(pessoa.cumprimentar())
+denis.OLHOS = 3
+Pessoa.OLHOS=4
+print(Pessoa.OLHOS)
+print(pessoa.OLHOS)
+print(denis.OLHOS)
+del denis.OLHOS
+print(denis.OLHOS)
